ce549fe84b620bf676f1205bc7f822212679ea31
Rahul Gandharva <rahulgandharva20122000@gmail.com>
2021-03-28 23:37:06 +1100

    Implemented KDE
    Created a log function that returns 0 if the input value is 0. Use this instead of regular log. PDF function now has 2 modes. classic mode and log mode. calculate_likelihood similarly has classic and kde modes. The pose class is now modified to hold it's data. Training with the kde mode, causes the data to be stored in the pose objects.

--- script.py
+++ script.py
@@ -1,14 +1,24 @@
 import pandas as pd
 import numpy as np
-from math import log
+from math import log, pi, sqrt, exp
+
+#Takes log, but returns 0, if the value is 0
+def log_0(x):
+	if x == 0:
+		return 0
+	else:
+		return log(x)
 
 #Takes a mean, variance and x value and returns the log of the density
 #Removed (sqrt(2*pi)) from the calcuation since it's a constant
-def pdf(normal, x):
+def pdf(normal, x, mode):
 	mean = normal.mean
 	stdev = normal.stdev
-	relative_sum = - log(stdev) - (1/2)*(((x-mean)/stdev)**2)
-	return relative_sum
+	if mode == "classic":
+		density = (1/(stdev*sqrt(2*pi))) * exp((-1/2)*(((x-mean)/stdev)**2))
+	if mode == "log":
+		density = -log_0(stdev*sqrt(2*pi)) -(0.5)*(((x-mean)/stdev)**2)
+	return density
 
 #Class the holds priors for each pose
 #Also holds the normal distributions for each attribute
@@ -17,15 +27,27 @@ class Pose:
 		self.name = name
 		self.prior = None
 		self.normals = []
+		self.data = None
 		
 	def __str__(self):
 		return f"Name: {self.name}, Prior: {self.prior}"
 		
-	def calculate_likelihood(self, instance):
+	def calculate_likelihood(self, instance, mode):
 		likelihood = 0
-		for normal, value in zip(self.normals, instance):
-			if not(np.isnan(value)):
-				likelihood += pdf(normal, value)
+		if mode == "classic":
+			for normal, attribute in zip(self.normals, instance):
+				if not(np.isnan(attribute)):
+					likelihood += pdf(normal, attribute, "log")
+					
+		if mode == "KDE":
+			for attribute, column_index in zip(instance, data.iloc[:,1:]):
+				if not(np.isnan(attribute)):
+					total_pdf = 0
+					for value in self.data[column_index]:
+						if not(np.isnan(value)):
+							total_pdf += pdf(Normal(value, 1), attribute, "classic")
+					likelihood += log_0(total_pdf/len(data))
+
 		return likelihood
 
 #Class that decribes a normal distribution with a certain mean and standard deviation	
@@ -45,29 +67,32 @@ def preprocess(filename):
 
 #Calculate priors and attribute distributions for a given dataframe
 #This dataframe should only hold data for a single class
-def calculate_model_info(group, num_instances):
+def calculate_model_info(group, num_instances, mode):
 	pose = Pose(group[0].iloc[0])
 	pose.prior = len(group[1])/num_instances
-	for mean, stdev in zip(group.mean(), group.std()):
-		pose.normals.append(Normal(mean, stdev))
+	if (mode == "classic"):
+		for mean, stdev in zip(group.mean(), group.std()):
+			pose.normals.append(Normal(mean, stdev))
+	if (mode == "KDE"):
+		pose.data = data.loc[data[0] == pose.name]
 	return pose
 
 #Training: Determining priors and attribute distributions for every class
 #Returns a pandas series that contains pose objects for every pose
 #Each object contains priors and attribute distributions
-def train(data):
+def train(data, mode):
 	groups = data.groupby([0])
-	poses = groups.apply(calculate_model_info, num_instances=len(data))
+	poses = groups.apply(calculate_model_info, num_instances=len(data), mode = mode)
 	return poses
 
 #Returns the name of the most likely post for any given instance
-def predict_instance(instance, poses):
-	likelihoods = [pose.calculate_likelihood(instance[1:]) for pose in poses]
+def predict_instance(instance, poses, mode):
+	likelihoods = [pose.calculate_likelihood(instance[1:], mode) for pose in poses]
 	return poses[np.argmax(likelihoods)].name
 
 #Predicts the class labels for a dataframe
-def predict(data, poses):
-	predictions = data.apply(predict_instance, poses = poses, axis = 1)
+def predict(data, poses, mode):
+	predictions = data.apply(predict_instance, poses = poses, mode = mode, axis = 1)
 	return predictions
 
 #Calculate accuracy of predictions
@@ -77,9 +102,9 @@ def evaluate(predictions, test):
 
 '''
 data = preprocess('train.csv')
-poses = train(data)
+poses = train(data, "KDE")
 instance = data.iloc[6]
-poses['bridge'].calculate_likelihood(list(instance[1:]))
-predictions = predict(data, poses)
+poses['bridge'].calculate_likelihood(list(instance[1:]), "KDE")
+predictions = predict(data, poses, "KDE")
 evaluate(predictions, data)
 '''
